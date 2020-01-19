a5f38917035b0021ea62ababb0379f59bbb5828a
Tania Batieva <yalyna.ts@gmail.com>
2020-01-19 19:33:38 +0200

    Fix env.example

--- etl/bq_client.py
+++ etl/bq_client.py
@@ -24,9 +24,6 @@ class BqClient:
         :param table_name: Name of table to be written, in the form dataset.tablename.
         :param df: DataFrame to be written to a Google BigQuery table.
         """
-        """Writes DataFrame to BigQuery.
-                By default used credentials specified in GOOGLE_APPLICATION_CREDENTIALS env variable.
-                """
         pandas_gbq.to_gbq(
             project_id=self.project_id, dataframe=df, destination_table=table_name
         )
